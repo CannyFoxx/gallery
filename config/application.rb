5aa7f92762b630388ba6ec0f4b1e71d1269e7122
Nikita Ignatovich <newrbu@gmail.com>
2017-07-13 17:17:39 -0700

    Correcting time zones, removed usuless puma config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Gallery
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Eastern Time (US & Canada)'
+
     config.action_view.embed_authenticity_token_in_remote_forms = true
     config.active_job.queue_adapter = :sidekiq
   end
